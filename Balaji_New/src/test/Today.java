f04e41dee0d80543829b5053512e3ce3deddb322
Vigneshwar-Natesan <replyvignesh92@gmail.com>
2025-10-22 13:17:31 +0530

    Second Push of Balaji Project

---
+++ Balaji_New/src/test/Today.java
@@ -0,0 +1,32 @@
+package test;
+
+public class Today {
+
+	public void add() {
+		int a=15;
+		int b=10;
+		System.out.println(a+b);
+	}
+	
+	public void sub() {
+		
+		double a=25.50;
+		int b=20;
+		System.out.println(a-b);
+	}
+	public static void main(String[] args) {
+		
+		Today c = new Today();
+		c.add();
+		c.sub();
+		int a=20;
+		int b=30;
+		//String c="Dinesh123$";
+		System.out.println(a+b);
+		System.out.println();
+		String name="balaji";
+		System.out.println(name);
+	
+		System.out.println("Hello");
+	}
+}
